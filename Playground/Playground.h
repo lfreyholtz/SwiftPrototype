3b38aaddccb01ac24d47bb08032cdd2bfc37ed24
lfreyholtz <lewfreyholtz@me.com>
2017-09-22 16:07:53 +0200

    adding playground

---
+++ Playground/Playground.h
@@ -0,0 +1,19 @@
+//
+//  Playground.h
+//  Playground
+//
+//  Created by LewFreyholtz on 9/22/17.
+//  Copyright © 2017 Lew Freyholtz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Playground.
+FOUNDATION_EXPORT double PlaygroundVersionNumber;
+
+//! Project version string for Playground.
+FOUNDATION_EXPORT const unsigned char PlaygroundVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Playground/PublicHeader.h>
+
+
